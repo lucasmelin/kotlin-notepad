b7ef8616a37d02265c64f452e2ce141cbe9d489d
Lucas Melin <lucas.melin@gmail.com>
2018-05-22 20:49:36 -0400

    Converted SQL statements to templated string

--- app/src/main/java/com/udacity/notepad/data/NotesContract.kt
+++ app/src/main/java/com/udacity/notepad/data/NotesContract.kt
@@ -18,18 +18,15 @@ object NotesContract {
         val UPDATED_AT = "updated_at"
     }
 
-    val SQL_CREATE_ENTRIES = "CREATE TABLE " + _TABLE_NAME + " (" +
-            _ID + " INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL, " +
-            TEXT + " TEXT, " +
-            IS_PINNED + " INTEGER, " +
-            CREATED_AT + " INTEGER, " +
-            UPDATED_AT + " INTEGER" +
-            ")"
+    val SQL_CREATE_ENTRIES = """CREATE TABLE $_TABLE_NAME (
+        $_ID INTEGER PRIMARY KEY AUTOINCREMENT NOT NULL,
+        $TEXT TEXT,
+        $IS_PINNED INTEGER,
+        $CREATED_AT INTEGER,
+        $UPDATED_AT INTEGER
+    )"""
 
     val SQL_DELETE_ENTRIES = "DROP TABLE IF EXISTS $_TABLE_NAME"
 
-    val SQL_QUERY_ALL =
-            "SELECT * FROM NOTE ORDER BY $CREATED_AT DESC"
-
-
+    val SQL_QUERY_ALL = "SELECT * FROM $_TABLE_NAME ORDER BY $CREATED_AT DESC"
 }
